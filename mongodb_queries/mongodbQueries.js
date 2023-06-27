10cb95e796bb5266c990a2870872befa534ae387
Konrad Winnicki <konrad.winnicki@protonmail.com>
2023-06-27 12:49:44 +0200

    Added MongoDB queries.

---
+++ mongodb_queries/mongodbQueries.js
@@ -0,0 +1,33 @@
+db.restaurants.find();
+db.restaurants.find({}, {restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
+db.restaurants.find({}, {restaurant_id: 1, name: 1, borough: 1, cuisine: 1, _id:0});
+db.restaurants.find({}, {restaurant_id: 1, name: 1, borough: 1, 'address.zipcode': 1, _id:0});
+db.restaurants.find({borough: 'Bronx'});
+db.restaurants.find({borough: 'Bronx'}).limit(5);
+db.restaurants.find({borough: 'Bronx'}).skip(5).limit(5);
+db.restaurants.find({'grades.score': {$gt: 90}});
+db.restaurants.find({grades:{$elemMatch: {score:{$gt: 80, $lt:100}}}})
+db.restaurants.find({'address.coord': {$eq: -95.754168}});
+db.restaurants.find({'grades.score': {$gt: 70},'cuisine': {$not: {$regex: 'American'}}, 'address.coord': {$eq: -65.754168}});
+db.restaurants.find({'grades.score': {$gt: 70},'cuisine': {$not: {$regex: 'American'}}, 'address.coord': {$lt: -65.754168}});
+db.restaurants.find({'grades.grade': 'A','cuisine': {$not: {$regex: 'American'}}, 'borough': {$not: {$regex: 'Brooklyn'}}});
+db.restaurants.find({name: {$regex: 'Wil'}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
+db.restaurants.find({name: {$regex: 'ces$'}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
+db.restaurants.find({name: {$regex: 'Reg'}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
+db.restaurants.find({borough: 'Brooklyn', $or: [{cuisine: 'American'}, {cuisine: 'Chinese'}]});
+db.restaurants.find({borough: {$in: ['Staten Island', 'Queens', 'Bronx', 'Brooklyn']}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
+db.restaurants.find({borough: {$nin: ['Staten Island', 'Queens', 'Bronx', 'Brooklyn']}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
+db.restaurants.find({'grades.score': {$lt:10}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
+db.restaurants.find({cuisine:{$regex: 'Seafood', $options: 'i'}, name: {$not:{$regex: 'Wil', $options: 'i'}}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1});
+db.restaurants.find({grades: {$elemMatch:{score: 11, grade: 'A',date: new Date('2014-08-11T00:00:00Z')}}}, {_id: 0, restaurant_id: 1, name: 1, grades: 1});
+db.restaurants.find({'grades.1.score': 9, 'grades.1.grade': 'A','grades.1.date': new Date('2014-08-11T00:00:00Z')}, {_id: 0, restaurant_id: 1, name: 1, grades: 1});
+db.restaurants.find({'address.coord.1': {$gte: 42, $lt: 52}}, {_id: 0, restaurant_id: 1, name: 1, address: 1});
+db.restaurants.find().sort({ name: 1 });
+db.restaurants.find().sort({ name: -1 });
+db.restaurants.aggregate([{$sort: { cuisine: 1, borough: -1 }}]);
+db.restaurants.find({'address.street': {$exists: true}});
+db.restaurants.find({'address.coord': {$type: 'double'}});
+db.restaurants.find({'grades.score': {$mod: [7,0]}});
+db.restaurants.find({name: {$regex: 'mon', $options: 'i'}}, {name:1, borough: 1, 'address.coord': 1, cuisine: 1, _id: 0});
+db.restaurants.find({name: {$regex: '^Mad', $options: 'i'}}, {name:1, borough: 1, 'address.coord': 1, cuisine: 1, _id: 0});
+
